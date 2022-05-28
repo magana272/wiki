5a01c47e988b655b3f078c474b3015f884c5b5b1
manuel <magana272@berkeley.edu>
2022-05-28 16:48:33 -0400

    Add Entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,5 @@
 
 from markdown2 import Markdown
-
 from webbrowser import get
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseRedirect, request
@@ -10,6 +9,12 @@ from . import util
 def to_index(request):
     return HttpResponseRedirect(reverse('index'))
 def index(request):
+    if request.method == "POST":
+        title= request.POST["pageName"]
+        content = request.POST["info"]
+        util.save_entry(title, content)
+        return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
+
     if request.method == "GET" and 'q' in request.GET:
         return HttpResponseRedirect(reverse("page",args=(request.GET.get('q', None),)))
     else: 
@@ -26,7 +31,6 @@ def page(request, page):
 
     elif(page_finder):
         markdowner = Markdown()
-        
         return render(request,"encyclopedia/page.html", {
                 "page":markdowner.convert(page_finder),
                 "pageName":page
---
+++ entries/Manuel Magana.md
@@ -0,0 +1,10 @@
+#The Best Software Engineer.
+
+##Linkedin :
+<a href='https://www.linkedin.com/in/manuel-magana-359290160' target="_blank">https://www.linkedin.com/in/manuel-magana-359290160/</a>
+
+##Github:
+<a href='https://github.com/magana272' target="_blank">https://github.com/magana272</a>
+
+##Personal Website: 
+<a href = 'https://magana272.github.io/' target="_blank"> https://magana272.github.io/</a>
